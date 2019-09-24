fd6014a331218ac4bcba310d6464dfe62e7b48c2
pierrezimmermann <pierre.zimmermann@student.ecp.fr>
2019-09-24 15:08:30 +0200

    Test de connexion

--- Console/PtitChat/Program.cs
+++ Console/PtitChat/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
 
 namespace PtitChat
 {
@@ -6,7 +9,33 @@ namespace PtitChat
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Voulez vous vous connecter ? (O/N)");
+            string reponse = Console.ReadLine();
+            if (reponse == "o" || reponse == "O")
+            {
+                Console.WriteLine("Rentrez une adresse ip et un port");
+                string IpAdress = Console.ReadLine();
+                TcpClient serveur = new TcpClient(IpAdress, 1302);
+            }
+            if (reponse == "n" || reponse == "N")
+                Console.WriteLine("ok");
+            else
+            {
+                Console.WriteLine("Erreur");
+                return;
+            }
+            Thread thread = new Thread(WaitConnexion);
+            thread.Start();
+                
+
+        }
+
+        public static void WaitConnexion()
+        {
+            TcpListener listener = new TcpListener(1302);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connecté");
         }
     }
 }
